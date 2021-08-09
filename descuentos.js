5326303820d5291dfd9019a2cdc1844e7730e1c5
Brian Rios <brianfrr90@gmail.com>
2021-08-08 22:20:51 -0500

    Se añaden archivos de HTML y JavaScript para calcular el precio con descuentos con cupones

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,21 @@
+// Código:
+
+const coupons = [
+    {
+        name: 'JuanDC_es_Batman', 
+        discount: 15,
+    },
+    {
+        name: 'pero_no_le_digas_a_nadie',
+        discount: 30,
+    },
+    {
+        name: 'es_un_secreto',
+        discount: 25,
+    },
+];
+
+
 function calcularPrecioConDescuento (precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento / 100);
@@ -10,15 +28,43 @@ function calcularPrecioConDescuento (precio, descuento) {
     return precioConDescuento;
 }
 
+
+// Interacción con HTML:
+
 function priceDiscountButton() {
+
     const inputPrice = document.getElementById('InputPrice');
     const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById('InputCoupon');
+    const couponValue = inputCoupon.value;    
 
-    const inputDiscount = document.getElementById('InputDiscount');
-    const discountValue = inputDiscount.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const isCouponValueValid  = function (coupon) {
+        return coupon.name === couponValue;
+    };
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+    
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + " no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    
+        const resultPrice = document.getElementById("ResultPrice");
+        resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }
 
-    const resultPrice = document.getElementById('ResultPrice');
-    resultPrice.innerText = 'El precio con descuento son: $' + precioConDescuento;
+    
+    // const inputPrice = document.getElementById('InputPrice');
+    // const priceValue = inputPrice.value;
+
+    // const inputCoupon = document.getElementById('InputCoupon');
+    // const couponValue = inputCoupon.value;
+
+    // const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    // const resultPrice = document.getElementById('ResultPrice');
+    // resultPrice.innerText = 'El precio con descuento son: $' + precioConDescuento;
 }
